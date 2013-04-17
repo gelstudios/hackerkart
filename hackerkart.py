6c9d9836b642f6a497613913f8bc3f14ee9b67b5
Eric Romano <gelstudios@gmail.com>
2013-04-16 22:42:10 -0400

    acceleration and turning works

--- hackerkart.py
+++ hackerkart.py
@@ -1,6 +1,6 @@
 #/usr/bin/env python
 
-import os, pygame, random
+import os, pygame, random, math
 from pygame.locals import *
 
 def load_image(name, colorkey=None):
@@ -25,8 +25,10 @@ class Kart(pygame.sprite.Sprite):
 
         
         self.speed = 0.0
-        self.max_speed = 20.0
-        self.accel_rate = 2.0
+        self.max_speed = 30.0
+        self.accel_rate = 1.0
+        self.brake_rate = 2.0
+        self.coast_rate = 1.05
         
         self.position = (0,0)
         self.last_position = (0,0)
@@ -41,27 +43,28 @@ class Kart(pygame.sprite.Sprite):
         self.spinning = 0
 
     def accel(self):
-        if self.speed <= self.max_speed:
+        if self.speed < self.max_speed:
             self.speed += self.accel_rate
-        print "speed " + repr(self.speed)
 
     def brake(self):
         if self.speed >= 1.0:
-            self.speed /= 2.0
+            self.speed /= self.brake_rate
         else:
             self.speed = 0
-        print "braking: " + repr(self.speed)
 
-    def decay(self):
-        self.brake()
-        self.turn_speed=0
+    def coast(self):
+        if self.speed >= 1.0:
+            self.speed /= self.coast_rate
+        else:
+            self.speed = 0
+        print "coasting"
 
     def turn(self, direction):
         if self.turn_speed < self.turn_max:
-            self.turn_speed += self.accel_rate
+            self.turn_speed += self.turn_rate
         self.last_heading = self.heading
         self.heading += self.turn_speed * direction
-        print "heading" + repr(self.heading)
+        self.heading %= 360
 
     def _spin(self):
         self.speed = 0
@@ -72,15 +75,20 @@ class Kart(pygame.sprite.Sprite):
         pass
 
     def _move(self):
-        #x, y = self.position
-        #vector = self.rect.pos() * self.speed
+        self.position = self.last_position
+        x, y = self.position
+        x = self.speed * math.cos(math.radians(self.heading))
+        y = self.speed * math.sin(math.radians(self.heading))
+        print "x: " + repr(x) + "y: " + repr(y)
+
+        newpos = self.rect.move((x, y))
+        self.rect = newpos
+
         center = self.rect.center
-        if self.heading >= 360:
-            self.heading -= 360
         rotate = pygame.transform.rotate
         self.image = rotate(self.original, self.heading)
         self.rect = self.image.get_rect(center=center)
-        print repr(self.rect) + repr(self.image)
+
 
     def update(self):
         if self.spinning:
@@ -127,8 +135,9 @@ def main():
 #Main Loop
     while 1:
         clock.tick(60)
-        print "speed: " + repr(kart.speed) + "-- heading: " + repr(kart.heading)
-        print pygame.key.get_pressed()
+        print "speed: " + repr(kart.speed) + " -- heading: " + repr(kart.heading) + " -- turn_speed: " + repr(kart.turn_speed)
+
+        keys = pygame.key.get_pressed()
 
     #Handle Input Events
         for event in pygame.event.get():
@@ -137,22 +146,27 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
             elif event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    kart.turn(1)
-                if event.key == K_RIGHT:
-                    kart.turn(-1)
-                if event.key == K_UP:
-                    kart.accel()
-                if event.key == K_DOWN:
-                    kart.brake()
-                else:
-                    kart.decay()
+                if event.key == K_a:
+                    kart.jump()
 
+        if keys[K_LEFT]:
+            kart.turn(1)
+        elif keys[K_RIGHT]:
+            kart.turn(-1)
+        else:
+            kart.turn_speed=0
+
+        if keys[K_UP]:
+            kart.accel()
+        elif keys[K_DOWN]:
+            kart.brake()
+        else:
+            kart.coast()
+            
 #       for racer in racers:
 #                   if track.terrain(racer):
 #                      chimp.punched()
 #                 else:
-
         allsprites.update()
 
     #Draw Everything
